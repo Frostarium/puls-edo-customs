de3c52f9c0116e770c131ef0dbf2f56dfc554cdc
Frostarium <193673661+Frostarium@users.noreply.github.com>
2025-05-26 19:43:56 -0500

    same thing

--- script/LostVerse/c5897604.lua
+++ script/LostVerse/c5897604.lua
@@ -25,10 +25,10 @@ function s.initial_effect(c)
     e4:SetRange(LOCATION_FZONE)
     e4:SetOperation(s.grantop)
     c:RegisterEffect(e4)
-    --Search or Special Summon
+    --Search
     local e5=Effect.CreateEffect(c)
     e5:SetDescription(aux.Stringid(id,1))
-    e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+    e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e5:SetType(EFFECT_TYPE_IGNITION)
     e5:SetRange(LOCATION_FZONE)
     e5:SetCountLimit(1)
@@ -39,10 +39,6 @@ function s.initial_effect(c)
 end
 
 local affected_cards={}
-
-function s.immtg(e,c)
-    return c:IsSetCard(0x5bc) and c:IsType(TYPE_NORMAL)
-end
 
 function s.stattg(e,c)
     return c:IsType(TYPE_NORMAL)
@@ -82,47 +78,17 @@ function s.srchfilter(c)
     return c:IsSetCard(0x5bc) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
 
-function s.spfilter(c,e,tp)
-    return c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-
 function s.srchtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local b1=Duel.IsExistingMatchingCard(s.srchfilter,tp,LOCATION_DECK,0,1,nil)
-    local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp)
-    if chk==0 then return b1 or b2 end
-    local op=0
-    if b1 and b2 then
-        op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
-    elseif b1 then
-        op=0
-    else
-        op=1
-    end
-    e:SetLabel(op)
-    if op==0 then
-        Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-    else
-        Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
-    end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.srchfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 
 function s.srchop(e,tp,eg,ep,ev,re,r,rp)
     if not e:GetHandler():IsRelateToEffect(e) then return end
-    local op=e:GetLabel()
-    if op==0 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local g=Duel.SelectMatchingCard(tp,s.srchfilter,tp,LOCATION_DECK,0,1,1,nil)
-        if #g>0 then
-            Duel.SendtoHand(g,nil,REASON_EFFECT)
-            Duel.ConfirmCards(1-tp,g)
-        end
-    else
-        if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
-        if #g>0 then
-            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-        end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.srchfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
     end
 end
