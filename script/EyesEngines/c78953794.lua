808baf5731d5367a516d733b7f43819db8185840
Frostarium <193673661+Frostarium@users.noreply.github.com>
2025-07-23 02:04:47 -0500

    test 3

--- script/EyesEngines/c78953794.lua
+++ script/EyesEngines/c78953794.lua
@@ -1,108 +1,94 @@
 --Futile Engine Y
 local s,id=GetID()
+local function target_filter(c)
+	return c:IsFaceup() or c:IsLocation(LOCATION_HAND) and c:IsMonster()
+end
+
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--Copy from hand/GY when opponent activates hand effect
+	--Copy and summon on trigger
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_HAND)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetHintTiming(0,TIMING_MAIN_END|TIMINGS_CHECK_MONSTER)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(function(e,tp) return Duel.GetCustomActivityCount(id,1-tp,ACTIVITY_CHAIN)>0 
-		or Duel.GetFlagEffect(1-tp,id)>0 end)
-	e2:SetTarget(s.copytg)
-	e2:SetOperation(s.copyop)
+	e2:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return eg:IsExists(Card.IsControler,1,nil,1-tp) and not Duel.IsPhase(PHASE_DRAW) end)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
-	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,s.chainfilter)
-	--Copy from Extra Deck
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetHintTiming(0,TIMING_END_PHASE)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCondition(s.spcon)
-	e3:SetCost(s.spcost)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_CHAIN_SOLVED)
+	e3:SetCondition(s.condition2)
 	c:RegisterEffect(e3)
-	--Track card additions
-	local ge1=Effect.CreateEffect(c)
-	ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	ge1:SetCode(EVENT_TO_HAND)
-	ge1:SetOperation(s.checkop)
-	Duel.RegisterEffect(ge1,0)
+    --Copy from Extra Deck
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetHintTiming(0,TIMING_END_PHASE)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCondition(s.spcon)
+	e4:SetCost(s.spcost)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
 end
 
-function s.chainfilter(re,tp,cid)
-    return not (re:GetActivateLocation()==LOCATION_HAND and re:IsMonsterEffect())
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp and re:IsMonsterEffect() and re:IsActivated() and re:GetActivateLocation()==LOCATION_HAND
 end
 
-function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-    if not re or re:GetHandler():IsCode(id) then return end
-    if ep~=tp and not Duel.GetCurrentPhase()==PHASE_DRAW then
-        Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,1)
-    end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
-function s.copyfilter(c)
-    return c:IsMonster() and c:IsFaceup()
-end
-
-function s.copytg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and (Duel.IsExistingMatchingCard(s.copyfilter,tp,0,LOCATION_MZONE,1,nil)
-        or Duel.IsExistingMatchingCard(s.copyfilter,tp,0,LOCATION_GRAVE,1,nil)) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
-end
-
-function s.copyop(e,tp,eg,ep,ev,re,r,rp)
-    if not e:GetHandler():IsRelateToEffect(e) then return end
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    local g1=Duel.GetMatchingGroup(s.copyfilter,tp,0,LOCATION_MZONE,nil)
-    local g2=Duel.GetMatchingGroup(s.copyfilter,tp,0,LOCATION_GRAVE,nil)
-    if #g1<=0 and #g2<=0 then return end
-    local sg=Group.CreateGroup()
-    local b1=#g1>0
-    local b2=#g2>0
-    local op=Duel.SelectEffect(tp,
-        {b1,aux.Stringid(id,2)},
-        {b2,aux.Stringid(id,3)})
-    if op==1 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-        sg=g1:Select(tp,1,1,nil)
-    else
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-        sg=g2:Select(tp,1,1,nil)
-    end
-    if #sg>0 then
-        local tc=sg:GetFirst()
-        local token=Duel.CreateToken(tp,tc:GetCode())
-        if Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)~=0 then
-            local e1=Effect.CreateEffect(e:GetHandler())
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_SET_BASE_ATTACK)
-            e1:SetValue(3000)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-            token:RegisterEffect(e1)
-            local e2=e1:Clone()
-            e2:SetCode(EFFECT_SET_BASE_DEFENSE)
-            token:RegisterEffect(e2)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g1=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	local g2=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	local g=g1:Clone()
+	g:Merge(g2)
+	if #g==0 then return end
+	Duel.ConfirmCards(tp,g1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local tc=g:Filter(target_filter,nil):Select(tp,1,1,nil):GetFirst()
+	if not tc then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local token=Duel.CreateToken(tp,tc:GetCode())
+	if Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_BASE_ATTACK)
+		e1:SetValue(3000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_BASE_DEFENSE)
+		token:RegisterEffect(e2)
+         --Copy Xyz materials if applicable
+            if token:IsType(TYPE_XYZ) and tc:IsType(TYPE_XYZ) and tc:GetOverlayCount()>0 then
+                local og=tc:GetOverlayGroup()
+                local mg=Group.CreateGroup()
+                for oc in aux.Next(og) do
+                    local mat=Duel.CreateToken(tp,oc:GetCode())
+                    if Duel.Remove(mat,POS_FACEUP,REASON_EFFECT)~=0 then
+                        mg:AddCard(mat)
+                    end
+                end
+                if #mg>0 then
+                    Duel.Overlay(token,mg)
+                end
+            end
         end
-    end
+	Duel.ShuffleHand(1-tp)
 end
-
-function s.namefilter(c,code)
-	return c:IsFaceup() and c:IsCode(code)
-end
-
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,nil)
 	local checked={}
